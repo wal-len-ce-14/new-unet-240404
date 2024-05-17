dc09b2f6e15da4e6105a352af05f11bbdf60dd94
wall_in_3080 <a33369097@gmail.com>
2024-05-17 17:21:57 +0800

    n

--- test_model.py
+++ test_model.py
@@ -13,31 +13,34 @@ device = "cpu"
 def test_res_model2(model_path, testingimgdir, file_name):
     imgdir = os.listdir(testingimgdir)
     image_batch = torch.empty(10,1,224,224)
-    image_O = np.zeros((10,1,224,224))
+    image_O = np.zeros((10,224,224,1))
     fig, axes = plt.subplots(1, 2, figsize=(4, 3))
     plt.setp(axes, xticks=[], yticks=[])
     model = torch.load(model_path).to(device=device)
 
-    for i, image in enumerate(imgdir):
-        image = os.path.join(testingimgdir, imgdir[i])
-        image = torch.tensor(np.array(cv.resize(cv.imread(image, cv.IMREAD_GRAYSCALE), (img_H, img_W)))).unsqueeze(0).unsqueeze(0)
-        image_O[i] = image.cpu().detach().numpy().squeeze()
+    for i, im in enumerate(imgdir):
+        image = os.path.join(testingimgdir, im)
+        image = torch.tensor(np.array(cv.resize(cv.imread(image, cv.IMREAD_GRAYSCALE), (img_H, img_W)))).unsqueeze(0)
+        # print(image.shape)
+        image_O[i] = image.cpu().permute(1,2,0).detach().numpy()
+        # print(image_O.shape)
         image_batch[i] = image
 
-    show = torch.sigmoid(model(image_batch.to(device=device, dtype=torch.float32)))
+    show1 = torch.sigmoid(model(image_batch.to(device=device, dtype=torch.float32)))
     # print(model(image.to(device=device, dtype=torch.float32)))
-    show = torch.where(show > 0.5, 1, 0).detach().numpy()
-    print(show)
+    show = torch.where(show1 > 0.5, 1, 0).detach().numpy()
+    # print(show)
     for i, m in enumerate(imgdir):
+        # print(image_O.shape)
         axes[0].imshow(image_O[i], cmap="gray")
         axes[0].set_title(f"Original Image\n{m.split('.')[0]}", fontsize=10)
         axes[1].imshow(image_O[i], cmap="gray")
         if(np.array_equal(show[i], [1,0])):
-            axes[1].set_title("Predict\nbenign", fontsize=10)
+            axes[1].set_title(f"Predict\n{show1[i][0]*100:.2f}%-benign", fontsize=10)
         elif(np.array_equal(show[i], [0,1])):
-            axes[1].set_title("Predict\nmalignant", fontsize=10)
-        plt.show()
-        # plt.savefig("plt-test/" + file_name + ".png")
+            axes[1].set_title(f"Predict\n{show1[i][1]*100:.2f}%-malignant", fontsize=10)
+        # plt.show()
+        plt.savefig("plt-test/" + file_name + f'_{i}' + ".png")
 
 
 
